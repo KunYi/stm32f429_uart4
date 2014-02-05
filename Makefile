62444b40c5f2fe31da50125baa02db0ccecdd40a
KunYi <kunyi_chen@(NB)>
2014-02-05 16:57:33 +0800

    currently just implement UART4 TX functions

---
+++ Makefile
@@ -0,0 +1,87 @@
+PROJECT = uart4
+
+EXECUTABLE = $(PROJECT).elf
+BIN_IMAGE = $(PROJECT).bin
+HEX_IMAGE = $(PROJECT).hex
+
+# set the path to STM32F429I-Discovery firmware package
+STDP ?= ../STM32F429I-Discovery_FW_V1.0.1
+
+CROSS_COMPILE = arm-none-eabi-
+
+CC = $(CROSS_COMPILE)gcc
+OBJCOPY = $(CROSS_COMPILE)objcopy
+OBJDUMP = $(CROSS_COMPILE)objdump
+SIZE = $(CROSS_COMPILE)size
+
+LIBS = -lm
+CFLAGS = -g -std=c99 -O3 -ffast-math
+CFLAGS += -mlittle-endian -mthumb
+CFLAGS += -mcpu=cortex-m4 -DSTM32F429_439xx
+#CFLAGS += -nostartfiles
+#CFLAGS += -ffreestanding
+#CFLAGS += -nostdlib
+CFLAGS += -Wl,--gc-sections
+
+# FPU
+CFLAGS += -mfpu=fpv4-sp-d16 -mfloat-abi=hard
+CFLAGS += -DARM_MATH_CM4 -D__FPU_PRESENT
+
+# to run from FLASH
+CFLAGS += -DVECT_TAB_FLASH
+CFLAGS += -Wl,-T,stm32f429zi_flash.ld
+
+# PROJECT SOURCE
+CFLAGS += -I.
+SOURCE = *.c
+
+# STARTUP FILE
+SOURCE += startup_stm32f429_439xx.S
+
+# CMSIS
+CFLAGS += -I$(STDP)/Libraries/CMSIS/Device/ST/STM32F4xx/Include
+CFLAGS += -I$(STDP)/Libraries/CMSIS/Include
+SOURCE += $(STDP)/Libraries/CMSIS/Lib/GCC/libarm_cortexM4lf_math.a
+
+# STemWinLibrary522_4x9i
+CFLAGS += -I$(STDP)/Libraries/STemWinLibrary522_4x9i/inc
+SOURCE += $(STDP)/Libraries/STemWinLibrary522_4x9i/Lib/STemWin522_4x9i_CM4_OS_GCC.a
+
+# STM32F4xx_StdPeriph_Driver
+CFLAGS += -DUSE_STDPERIPH_DRIVER
+CFLAGS += -I$(STDP)/Libraries/STM32F4xx_StdPeriph_Driver/inc
+
+# STM32F429I-Discovery Utilities
+CFLAGS += -I$(STDP)/Utilities/STM32F429I-Discovery
+SOURCE += $(STDP)/Utilities/STM32F429I-Discovery/stm32f429i_discovery.c
+SOURCE += $(STDP)/Utilities/STM32F429I-Discovery/stm32f429i_discovery_l3gd20.c
+SOURCE += $(STDP)/Utilities/STM32F429I-Discovery/stm32f429i_discovery_lcd.c
+SOURCE += $(STDP)/Utilities/STM32F429I-Discovery/stm32f429i_discovery_sdram.c
+
+all: libSTM32F4xx_StdPeriph_Driver.a $(BIN_IMAGE)
+
+$(BIN_IMAGE): $(EXECUTABLE)
+	$(OBJCOPY) -O binary $^ $@
+	$(OBJCOPY) -O ihex $^ $(HEX_IMAGE)
+	$(OBJDUMP) -h -S -D $(EXECUTABLE) > $(PROJECT).lst
+	$(SIZE) $(EXECUTABLE)
+	
+$(EXECUTABLE): $(SOURCE)
+	$(CC) $(CFLAGS) $^ -o $@ libSTM32F4xx_StdPeriph_Driver.a $(LIBS)
+	
+libSTM32F4xx_StdPeriph_Driver.a: 
+	$(CC) $(CFLAGS) -c -D"assert_param(expr)=((void)0)" $(STDP)/Libraries/STM32F4xx_StdPeriph_Driver/src/*.c
+	@$(AR) cr $@ *.o 
+	rm -rf *.o
+
+clean:
+	rm -rf $(EXECUTABLE)
+	rm -rf $(BIN_IMAGE)
+	rm -rf $(HEX_IMAGE)
+	rm -f libSTM32F4xx_StdPeriph_Driver.a
+	rm -f $(PROJECT).lst
+
+flash:
+	st-flash write $(BIN_IMAGE) 0x8000000
+
+.PHONY: clean
---
+++ main.c
@@ -0,0 +1,131 @@
+/**
+  ******************************************************************************
+  * @file    Template/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    20-September-2013
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup Template
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+void Delay(__IO uint32_t nCount) {
+    while(nCount--) {
+    }
+}
+
+void init_UART4(uint32_t baudrate) {
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_UART4);
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource1, GPIO_AF_UART4);
+
+    USART_InitTypeDef USART_InitStruct;
+    USART_InitStruct.USART_BaudRate = baudrate;
+    USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+    USART_InitStruct.USART_StopBits = USART_StopBits_1;
+    USART_InitStruct.USART_Parity = USART_Parity_No;
+    USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+    USART_Init(UART4, &USART_InitStruct);
+
+    USART_Cmd(UART4, ENABLE);
+}
+
+void UART4_puts(USART_TypeDef* USARTx, volatile char *s) {
+    while(*s) {
+        while (!(USARTx->SR & 0x40));
+        USART_SendData(USARTx, *s);
+        s++;
+    }
+}
+/**
+  * @brief   Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       files (startup_stm32f429_439xx.s) before to branch to application main. 
+       To reconfigure the default setting of SystemInit() function, refer to
+       system_stm32f4xx.c file
+     */  
+  
+  /* Add your application code here */
+  init_UART4(115200);
+  UART4_puts(UART4, "Hello World!\r\n");
+    
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
